baff9303a8acca009b2944499b018ea30962fccd
carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-20 22:42:58 -0400

    Fix comment formatting in CriptoSearchForm component

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -8,7 +8,7 @@ export default function CriptoSearchForm() {
         <div className="field">
             <label htmlFor="currency">Moneda:</label>
             <select 
-                name="currency" //este name es el que me va ayudar a escribir el valor en el state - para seleccionar lo que quiero cotizar hola
+                name="currency" //este name es el que me va ayudar a escribir el valor en el state - para seleccionar lo que quiero cotizar
                 id="currency"
             >
                 <option value="">-- Seleccione --</option>
