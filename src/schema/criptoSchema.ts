0d82d06af607142ceaa7a207436eb5da0d8147df
carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-22 22:43:26 -0400

    Refactor criptoSchema and store: improve comments and formatting for clarity

--- src/schema/criptoSchema.ts
+++ src/schema/criptoSchema.ts
@@ -8,9 +8,11 @@ export const CurrencySchema = z.object({
 //Cuando haces la consulta y vez que esta llegando la llamada de la api, verifica que data recibes o puedes ver
 // y empiezas a crear el schema de lo que realmente quieres extraer de todos los datos que vez de la API
 
-export const CryptoCurrencyResponseSchema = z.object({
+export const CryptoCurrencyResponseSchema =  z.object({
     CoinInfo : z.object({
         FullName: z.string(),
         Name: z.string()
     })
 })
+
+export const CryptoCurrenciesResponseSchema = z.array(CryptoCurrencyResponseSchema)//Es un array de objetos que cumplen con el esquema CryptoCurrencyResponseSchema
--- src/store.ts
+++ src/store.ts
@@ -2,21 +2,20 @@
 
 import {create} from 'zustand';
 import axios from 'axios';
+import { CryptoCurrenciesResponseSchema } from './schema/criptoSchema';
 
 
 async function getCryptos() {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
     const {data: {Data}} = await axios.get(url)//Desestructurando la respuesta de axios para obtener solo el objeto Data que esta dentro de data.
-    console.log(Data)
+    //console.log(Data)
+    const result = CryptoCurrenciesResponseSchema.safeParse(Data)//safeParse es una función de zod que valida los datos según el esquema definido.
+    console.log(result)
 }   
 
 
-
-
 //create: Crea un store de Zustand.
-
 //useCryptoStore: Es un hook personalizado que cualquier componente puede usar para acceder al estado o funciones del store.
-
 //fetchCryptos: Una función asíncrona que (por ahora) solo imprime un mensaje.
 
 export const useCryptoStore = create(()=>({
