a9a5eaeb7a7deec79b7f72b58d6f83cd54387f49
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-04-28 10:37:01 -0400

    Refactorizar store: mover getCryptos función a services

--- src/store.ts
+++ src/store.ts
@@ -2,30 +2,20 @@
 
 import {create} from 'zustand';
 import {devtools } from 'zustand/middleware';
-import axios from 'axios';
-import { CryptoCurrenciesResponseSchema } from './schema/criptoSchema';
 import { Cryptocurrency } from "./types";
+import { getCryptos } from './services/CryptoService';//Importamos la función getCryptos que hace la llamada a la API para obtener las criptomonedas.
 
 type CryptoStore = {
   cryptoCurrencies: Cryptocurrency[];
   fetchCryptos: () => Promise<void>;
 };
 
-async function getCryptos() {
-    const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
-    const {data: {Data}} = await axios.get(url)//Desestructurando la respuesta de axios para obtener solo el objeto Data que esta dentro de data.
-    //console.log(Data)
-    const result = CryptoCurrenciesResponseSchema.safeParse(Data)//safeParse es una función de zod que valida los datos según el esquema definido.
-    //console.log(result)
-    if(result.success){
-        return result.data
-    }
-}   
+
 
 
 //create: Crea un store de Zustand.
 //useCryptoStore: Es un hook personalizado que cualquier componente puede usar para acceder al estado o funciones del store.
-//fetchCryptos: Una función asíncrona que (por ahora) solo imprime un mensaje.
+//fetchCryptos: Es una función que se encarga de obtener las criptomonedas desde la API y actualizar el estado del store con los datos obtenidos.
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set)=>({
     cryptoCurrencies: [],//Estado inicial del store, un array vacío.
