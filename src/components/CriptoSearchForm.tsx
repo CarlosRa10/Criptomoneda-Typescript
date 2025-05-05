9ace82b57eba7c81ae91baf62ee5ba7282a0d8f6
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-05 17:21:49 -0400

    Refactorizar CriptoSearchForm: agregar gestión de estado para la selección de moneda y actualizar PairSchema en el esquema

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,8 +1,20 @@
 import { useCryptoStore } from "../store"
 import { currencies } from "../data"
+import { ChangeEvent, useState } from "react"
+import { Pair } from "../types"
 
 export default function CriptoSearchForm() {
     const cryptocurrencies = useCryptoStore((state)=>state.cryptoCurrencies)//llamamos al store para obtener las criptomonedas y poder usarlas en el select de criptomonedas.
+    const [pair, setPair] = useState<Pair>({
+        currency: '',
+        criptocurrency: ''
+    })
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [e.target.name]: e.target.value //actualizamos el state con el valor del select que se ha cambiado
+        })
+    }
     return (
         <form
             className="form"
@@ -12,6 +24,7 @@ export default function CriptoSearchForm() {
                 <select 
                     name="currency" //este name es el que me va ayudar a escribir el valor en el state - para seleccionar lo que quiero cotizar
                     id="currency"
+                    onChange ={handleChange}
                 >
                     <option value="">-- Seleccione --</option>
                     {currencies.map(currency => (
@@ -30,6 +43,7 @@ export default function CriptoSearchForm() {
                 <select 
                     name="criptocurrency"
                     id ="criptocurrency" 
+                    onChange ={handleChange}
                 >
                     <option value="">-- Seleccione --</option>
                     {cryptocurrencies.map(crypto => (
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod'//usaremos la función infer de zod para inferir el tipo de datos de un objeto
-import {CurrencySchema,CryptoCurrencyResponseSchema} from '../schema/criptoSchema'
+import {CurrencySchema,CryptoCurrencyResponseSchema, PairSchema} from '../schema/criptoSchema'
 
 export type Currency = z.infer<typeof CurrencySchema> //inferimos el tipo de datos de un objeto que cumple con el esquema definido en criptoSchema.ts
 export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema> //inferimos el tipo de datos de un objeto que cumple con el esquema definido en criptoSchema.ts
+export type Pair = z.infer<typeof PairSchema> //inferimos el tipo de datos de un objeto que cumple con el esquema definido en criptoSchema.ts
