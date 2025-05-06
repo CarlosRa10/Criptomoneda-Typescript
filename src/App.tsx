e4ffca2d7c359a88fd3700c597e1686af2d2c6de
Carlosra10 <carloseduardoramirez10@gmail.com>
2025-05-06 15:59:02 -0400

    Agregar componente CryptoPriceDisplay y actualizar App para mostrar la cotización de criptomonedas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react"
 import CriptoSearchForm from "./components/CriptoSearchForm"
 import { useCryptoStore } from "./store"
+import CryptoPriceDisplay from "./components/CryptoPriceDisplay"
 
 
 
@@ -20,6 +21,7 @@ function App() {
 
           <div className="content">
               <CriptoSearchForm />
+              <CryptoPriceDisplay/>
           </div>
       </div>
     </>
---
+++ src/components/CryptoPriceDisplay.tsx
@@ -0,0 +1,29 @@
+import { useMemo } from "react"
+import { useCryptoStore } from "../store"
+
+export default function CryptoPriceDisplay() {
+    const result = useCryptoStore((state) => state.result)//llamamos al store para obtener el resultado de la API y poder usarlo en el componente.
+    const hasResult = useMemo(() => !Object.values(result).includes('') , [result] )
+    //useMemo: memoriza el resultado de la función y solo se vuelve a calcular si el valor de result cambia.
+    //Object.values(result):Convierte los valores del objeto result en un array. Ejemplo: Si result = { a: 1, b: '', c: 'hola' }
+    //!Object.values(result).includes('') : verifica si hay un valor vacio en el resultado de la API. Si no hay un valor vacio, devuelve true.
+  return (
+    <div className="result-wrapper">
+        {hasResult && (
+            <>
+                <h2>Cotización</h2>
+                <div className="result">
+                    <img src={`https://www.cryptocompare.com${result.IMAGEURL}`} alt="Logo de la criptomoneda" />
+                    <div>
+                        <p>El precio es de: <span>{result.PRICE}</span></p>
+                        <p>Precio más alto del día: <span>{result.HIGHDAY}</span></p>
+                        <p>Precio más bajo del día: <span>{result.LOWDAY}</span></p>
+                        <p>Variación ultimas 24 horas: <span>{result.CHANGEPCT24HOUR}</span></p>
+                        <p>Última actualización: <span>{result.LASTUPDATE}</span></p>
+                    </div>
+                </div>
+            </>
+        )}
+    </div>
+  )
+}
